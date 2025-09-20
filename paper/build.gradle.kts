4349cfc7abe13f98011109cda6f62fcdd511ff8a
ruskei <dominusfinem@gmail.com>
2025-09-19 17:00:57 -0700

    open provider

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `maven-publish`
     signing
+    kotlin("jvm") version "2.1.21"
 }
 
 repositories {
@@ -15,6 +16,7 @@ dependencies {
     api(project(":triumph-gui")) {
         exclude(group = "net.kyori")
     }
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 license {
@@ -27,8 +29,6 @@ license {
 val javaComponent: SoftwareComponent = components["java"]
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
     disableAutoTargetJvm()
 }
 
@@ -45,85 +45,10 @@ tasks {
         from(javadoc)
     }
 
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                from(javaComponent)
-                artifact(sourcesJar)
-                artifact(javadocJar)
-                versionMapping {
-                    usage("java-api") {
-                        fromResolutionOf("runtimeClasspath")
-                    }
-                    usage("java-runtime") {
-                        fromResolutionResult()
-                    }
-                }
-                pom {
-                    name.set("Triumph GUI")
-                    description.set("Library for easy creation of GUIs for Bukkit plugins.")
-                    url.set("https://github.com/TriumphTeam/triumph-gui")
-
-                    licenses {
-                        license {
-                            name.set("MIT License")
-                            url.set("http://www.opensource.org/licenses/mit-license.php")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.set("matt")
-                            name.set("Mateus Moreira")
-                            organization.set("TriumphTeam")
-                            organizationUrl.set("https://github.com/TriumphTeam")
-                        }
-                    }
-
-                    scm {
-                        connection.set("scm:git:git://github.com/TriumphTeam/triumph-gui.git")
-                        developerConnection.set("scm:git:ssh://github.com:TriumphTeam/triumph-gui.git")
-                        url.set("https://github.com/TriumphTeam/triumph-gui")
-                    }
-                }
-            }
-        }
-
-        repositories {
-            maven {
-                if (version.toString().contains("SNAPSHOT")) {
-                    credentials {
-                        username = project.providers.gradleProperty("triumph.repo.user").get()
-                        password = project.providers.gradleProperty("triumph.repo.token").get()
-                    }
-
-                    url = uri("https://repo.triumphteam.dev/snapshots/")
-                    return@maven
-                }
-
-                credentials {
-                    username = project.providers.gradleProperty("ossrh.username").get()
-                    password = project.providers.gradleProperty("ossrh.password").get()
-                }
-
-                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            }
-        }
-
-    }
-
-    signing {
-        /*useGpgCmd()
-        val signingKey = System.getenv("GPG_KEY")
-        val signingPassword = System.getenv("GPG_PASS")
-        val secretKey = System.getenv("GPG_SECRET_KEY")
-        useInMemoryPgpKeys(signingKey, secretKey, signingPassword)*/
-        sign(publishing.publications["maven"])
-    }
-
     withType<JavaCompile> {
-        sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-        targetCompatibility = JavaVersion.VERSION_1_8.toString()
         options.encoding = "UTF-8"
     }
+}
+kotlin {
+    jvmToolchain(21)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,9 @@ pluginManagement {
         kotlin("jvm") version "1.9.10"
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 rootProject.name = "triumph-gui"
 
 include("core")
